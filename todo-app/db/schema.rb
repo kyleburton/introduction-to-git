f42b2995007c5bc175aa38e14b7360e9f2e16c54
Kyle Burton <kyle.burton@gmail.com>
2009-05-05 09:45:05 -0400

    remove broken scaffold

--- todo-app/db/schema.rb
+++ todo-app/db/schema.rb
@@ -9,14 +9,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090505130924) do
-
-  create_table "items", :force => true do |t|
-    t.string   "category"
-    t.text     "item"
-    t.boolean  "completed"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(:version => 0) do
 
 end
